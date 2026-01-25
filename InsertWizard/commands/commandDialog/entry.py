b9f34b54c3a98e80613d8fa2270acd31d7abbe05
Rene Triebenstein <info@know-how-schmiede.de>
2026-01-25 17:28:55 +0100

    Add versioning and enhance command dialog functionality
    Introduces a version.py file for version tracking and updates the command dialog to display the version in the title. The dialog now allows selection of multiple sketch points (with validation for same-sketch selection), and adds diameter and depth inputs for extrusion. The command logic is updated to extrude circles at selected points, cutting into target bodies, and input validation is improved. Removes unused palette commands from __init__.py. Adds version.md for version history.

--- InsertWizard/commands/commandDialog/entry.py
+++ InsertWizard/commands/commandDialog/entry.py
@@ -1,15 +1,17 @@
 import adsk.core
+import adsk.fusion
 import os
 from ...lib import fusionAddInUtils as futil
 from ... import config
+from ... import version
 app = adsk.core.Application.get()
 ui = app.userInterface
 
 
 # TODO *** Specify the command identity information. ***
 CMD_ID = f'{config.COMPANY_NAME}_{config.ADDIN_NAME}_cmdDialog'
-CMD_NAME = 'Command Dialog Sample'
-CMD_Description = 'A Fusion Add-in Command with a dialog'
+CMD_NAME = f'InsertWizard v{version.__version__}'
+CMD_Description = 'InsertWizard Dialog'
 
 # Specify that the command will be promoted to the panel.
 IS_PROMOTED = True
@@ -19,8 +21,7 @@ IS_PROMOTED = True
 # command it will be inserted beside. Not providing the command to position it
 # will insert it at the end.
 WORKSPACE_ID = 'FusionSolidEnvironment'
-PANEL_ID = 'SolidScriptsAddinsPanel'
-COMMAND_BESIDE_ID = 'ScriptsManagerCommand'
+PANEL_ID = 'SolidCreatePanel'
 
 # Resource location for command icons, here we assume a sub folder in this directory named "resources".
 ICON_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', '')
@@ -46,7 +47,7 @@ def start():
     panel = workspace.toolbarPanels.itemById(PANEL_ID)
 
     # Create the button command control in the UI after the specified existing command.
-    control = panel.controls.addCommand(cmd_def, COMMAND_BESIDE_ID, False)
+    control = panel.controls.addCommand(cmd_def)
 
     # Specify if the command is promoted to the main toolbar. 
     control.isPromoted = IS_PROMOTED
@@ -78,20 +79,29 @@ def command_created(args: adsk.core.CommandCreatedEventArgs):
     # https://help.autodesk.com/view/fusion360/ENU/?contextId=CommandInputs
     inputs = args.command.commandInputs
 
-    # TODO Define the dialog for your command by adding different inputs to the command.
+    # Show a simple dialog that displays the add-in name.
+    inputs.addTextBoxCommandInput('title', '', CMD_NAME, 1, True)
 
-    # Create a simple text box input.
-    inputs.addTextBoxCommandInput('text_box', 'Some Text', 'Enter some text.', 1, False)
+    # Selection input for one or more sketch points.
+    points_input = inputs.addSelectionInput('points', 'Punkte', 'Skizzenpunkte auswählen')
+    points_input.addSelectionFilter('SketchPoints')
+    points_input.setSelectionLimits(1, 0)
+    try:
+        points_input.isMultiSelectEnabled = True
+    except:
+        # Older API versions may not expose this property.
+        pass
 
-    # Create a value input field and set the default using 1 unit of the default length unit.
-    defaultLengthUnits = app.activeProduct.unitsManager.defaultLengthUnits
-    default_value = adsk.core.ValueInput.createByString('1')
-    inputs.addValueInput('value_input', 'Some Value', defaultLengthUnits, default_value)
+    # Diameter input in mm with a default of 10 mm.
+    default_diameter = adsk.core.ValueInput.createByString('10 mm')
+    inputs.addValueInput('diameter', 'Durchmesser 1', 'mm', default_diameter)
 
-    # TODO Connect to the events that are needed by this command.
+    # Depth input in mm.
+    default_depth = adsk.core.ValueInput.createByString('5 mm')
+    inputs.addValueInput('depth', 'Tiefe', 'mm', default_depth)
+
+    # Connect to the events that are needed by this command.
     futil.add_handler(args.command.execute, command_execute, local_handlers=local_handlers)
-    futil.add_handler(args.command.inputChanged, command_input_changed, local_handlers=local_handlers)
-    futil.add_handler(args.command.executePreview, command_preview, local_handlers=local_handlers)
     futil.add_handler(args.command.validateInputs, command_validate_input, local_handlers=local_handlers)
     futil.add_handler(args.command.destroy, command_destroy, local_handlers=local_handlers)
 
@@ -102,35 +112,131 @@ def command_execute(args: adsk.core.CommandEventArgs):
     # General logging for debug.
     futil.log(f'{CMD_NAME} Command Execute Event')
 
-    # TODO ******************************** Your code here ********************************
-
-    # Get a reference to your command's inputs.
     inputs = args.command.commandInputs
-    text_box: adsk.core.TextBoxCommandInput = inputs.itemById('text_box')
-    value_input: adsk.core.ValueCommandInput = inputs.itemById('value_input')
+    points_input: adsk.core.SelectionCommandInput = inputs.itemById('points')
+    diameter_input: adsk.core.ValueCommandInput = inputs.itemById('diameter')
+    depth_input: adsk.core.ValueCommandInput = inputs.itemById('depth')
 
-    # Do something interesting
-    text = text_box.text
-    expression = value_input.expression
-    msg = f'Your text: {text}<br>Your value: {expression}'
-    ui.messageBox(msg)
+    if points_input.selectionCount < 1:
+        ui.messageBox('Bitte mindestens einen Skizzenpunkt auswählen.')
+        return
 
+    design = adsk.fusion.Design.cast(app.activeProduct)
+    if not design:
+        ui.messageBox('Kein aktives Design gefunden.')
+        return
 
-# This event handler is called when the command needs to compute a new preview in the graphics window.
-def command_preview(args: adsk.core.CommandEventArgs):
-    # General logging for debug.
-    futil.log(f'{CMD_NAME} Command Preview Event')
-    inputs = args.command.commandInputs
+    radius = diameter_input.value / 2.0
+    depth_value = abs(depth_input.value)
 
+    selection_count = points_input.selectionCount
+    futil.log(f'Ausgewählte Punkte: {selection_count}', force_console=True)
 
-# This event handler is called when the user changes anything in the command dialog
-# allowing you to modify values of other inputs based on that change.
-def command_input_changed(args: adsk.core.InputChangedEventArgs):
-    changed_input = args.input
-    inputs = args.inputs
+    centers = []
+    base_sketch = None
+    mismatched_sketch = False
+    for i in range(selection_count):
+        try:
+            selection = points_input.selection(i)
+        except:
+            futil.log(f'Ungültiger Auswahlindex: {i}', force_console=True)
+            break
+        entity = selection.entity
+        if not entity:
+            continue
+        if not hasattr(entity, 'parentSketch') or not hasattr(entity, 'geometry'):
+            continue
 
-    # General logging for debug.
-    futil.log(f'{CMD_NAME} Input Changed Event fired from a change to {changed_input.id}')
+        sketch = entity.parentSketch
+        if not base_sketch:
+            base_sketch = sketch
+        elif sketch != base_sketch:
+            mismatched_sketch = True
+            break
+        center = entity.geometry
+        centers.append((sketch, adsk.core.Point3D.create(center.x, center.y, center.z)))
+        futil.log(
+            f'Punkt {i + 1}: X={center.x:.3f} Y={center.y:.3f} Z={center.z:.3f}',
+            force_console=True
+        )
+
+    if mismatched_sketch:
+        ui.messageBox('Bitte nur Punkte aus derselben Skizze auswählen.')
+        return
+
+    if not centers:
+        return
+
+    base_sketch = centers[0][0]
+    plane_entity = None
+    if hasattr(base_sketch, 'referencePlane') and base_sketch.referencePlane:
+        plane_entity = base_sketch.referencePlane
+    elif hasattr(base_sketch, 'planarEntity') and base_sketch.planarEntity:
+        plane_entity = base_sketch.planarEntity
+
+    target_sketch = base_sketch
+
+    # Determine target bodies for the cut.
+    target_bodies = adsk.core.ObjectCollection.create()
+    face = adsk.fusion.BRepFace.cast(plane_entity)
+    if face:
+        target_bodies.add(face.body)
+    else:
+        for body in target_sketch.parentComponent.bRepBodies:
+            if body.isSolid:
+                target_bodies.add(body)
+
+    if target_bodies.count == 0:
+        futil.log('Kein Zielkörper im Component gefunden.', force_console=True)
+        return
+
+    for _, center in centers:
+        circle = target_sketch.sketchCurves.sketchCircles.addByCenterRadius(center, radius)
+
+        profile = None
+        fallback_profile = None
+        for prof in target_sketch.profiles:
+            for loop in prof.profileLoops:
+                for curve in loop.profileCurves:
+                    if curve.sketchEntity == circle:
+                        if hasattr(loop, 'isOuter') and loop.isOuter:
+                            profile = prof
+                            break
+                        fallback_profile = prof
+                if profile:
+                    break
+            if profile:
+                break
+
+        if not profile:
+            profile = fallback_profile
+
+        if not profile:
+            futil.log('Kein Profil für Kreis gefunden.', force_console=True)
+            continue
+
+        extrudes = target_sketch.parentComponent.features.extrudeFeatures
+        success = False
+        for is_positive in (True, False):
+            ext_input = extrudes.createInput(profile, adsk.fusion.FeatureOperations.CutFeatureOperation)
+            ext_input.setDistanceExtent(False, adsk.core.ValueInput.createByReal(depth_value))
+            try:
+                ext_input.participantBodies = target_bodies
+            except:
+                pass
+            if hasattr(ext_input, 'isPositiveDirection'):
+                ext_input.isPositiveDirection = is_positive
+            elif not is_positive:
+                ext_input.setDistanceExtent(False, adsk.core.ValueInput.createByReal(-depth_value))
+            try:
+                extrudes.add(ext_input)
+                success = True
+                break
+            except:
+                continue
+
+        if not success:
+            futil.log('Kein Zielkörper zum Schneiden gefunden.', force_console=True)
 
 
 # This event handler is called when the user interacts with any of the inputs in the dialog
@@ -140,14 +246,15 @@ def command_validate_input(args: adsk.core.ValidateInputsEventArgs):
     futil.log(f'{CMD_NAME} Validate Input Event')
 
     inputs = args.inputs
-    
-    # Verify the validity of the input values. This controls if the OK button is enabled or not.
-    valueInput = inputs.itemById('value_input')
-    if valueInput.value >= 0:
-        args.areInputsValid = True
-    else:
+    points_input: adsk.core.SelectionCommandInput = inputs.itemById('points')
+    diameter_input: adsk.core.ValueCommandInput = inputs.itemById('diameter')
+    depth_input: adsk.core.ValueCommandInput = inputs.itemById('depth')
+
+    if points_input.selectionCount < 1 or diameter_input.value <= 0 or depth_input.value <= 0:
         args.areInputsValid = False
-        
+        return
+
+    args.areInputsValid = True
 
 # This event handler is called when the command terminates.
 def command_destroy(args: adsk.core.CommandEventArgs):
---
+++ version.md
@@ -0,0 +1,9 @@
+# Versionen InsertWizard
+
+## Version 0.0.1 - 2026-01-25
+
+- erste Lauffähige Version
+- Version in Titelzeile
+- mehrere Punkte auf Skizze anwählbar
+- Überprüfung nur Punkte auf gleicher Skizze
+- Extrusion der ausgewählten Kreise bzw. Punkte
